72c6d37eea7e8d034da0e335fb807690c5a027cf
Evgeniy Demeshko <evg.education.and.work@gmail.com>
2025-10-22 20:18:55 +0000

    Lab7: added styles to card with tailwind

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,12 @@ import { defineConfig } from 'vite';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url'
 import handlebars from 'vite-plugin-handlebars'
-import tailwindcss from '@tailwindcss/vite';
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname  = dirname(__filename)
 
 export default defineConfig({
     plugins: [
-        tailwindcss(),
         handlebars({
             partialDirectory: resolve(__dirname, 'src', 'partials'),
             context: { siteName: 'Лабораторна 7', labName: 'Лабораторна № 7' },
